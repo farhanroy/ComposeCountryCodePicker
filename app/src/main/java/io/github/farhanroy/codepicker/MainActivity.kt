78e5e750e106f532d4f048bf0ea608e9940e25e1
Farhan Roy <farhanroy120@gmail.com>
2021-09-24 19:13:42 +0700

    .

--- app/src/main/java/io/github/farhanroy/codepicker/MainActivity.kt
+++ app/src/main/java/io/github/farhanroy/codepicker/MainActivity.kt
@@ -3,8 +3,17 @@ package io.github.farhanroy.codepicker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import io.github.farhanroy.cccp.CountryCodeDialog
 import io.github.farhanroy.codepicker.ui.theme.ComposeCountryCodePickerTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,9 +23,21 @@ class MainActivity : ComponentActivity() {
             ComposeCountryCodePickerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-
+                    CountryCodeView()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun CountryCodeView() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        var country by remember { mutableStateOf("") }
+        CountryCodeDialog {ccpCountry ->
+            country = ccpCountry.name
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Text("")
     }
 }
